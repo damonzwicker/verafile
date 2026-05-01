ce7f10b3cd214278b234929d687fb0691a6e1997
Damon Zwicker <dzwicker@me.com>
2026-04-30 21:17:08 -0700

    Export verify function + index entry

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.verify = void 0;
+var verify_1 = require("./verify");
+Object.defineProperty(exports, "verify", { enumerable: true, get: function () { return verify_1.verify; } });
